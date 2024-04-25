9cc652b0f7a6952176e935857a66f7f02f9eee3c
shahabuddin135 <voyagersvrs135@gmail.com>
2024-04-25 23:20:31 +0500

    version 1

---
+++ index.ts
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bgBlueBright("\n\t\tWELCOME TO OTP GENERATOR\t\n"));
+
+
+while(true){
+let answer = await inquirer.prompt(
+
+  {
+    message:chalk.magentaBright( "Do you want to Generate OTP?"),
+    name:"confirm",
+    type:"confirm",
+  }
+);
+
+let otp = Math.floor( Math.random() * 900000 + 100000);
+
+
+
+if(answer.confirm === false){
+  console.log(chalk.bgRedBright("\n\t\tBYE BYE\t\t\n"));
+  break;
+}
+
+console.log(chalk.greenBright(otp));
+
+
+}
